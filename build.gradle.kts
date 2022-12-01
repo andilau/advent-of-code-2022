5cc3abd61030036a4f1320863ab9642a5452079c
Andreas Lau <andreas.lau@europace.de>
2022-12-01 21:17:24 +0100

    Set jvmTarget to 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
 
 tasks {
     withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "1.8"
         kotlinOptions.freeCompilerArgs = listOf("-Xjsr305=strict")
     }
     test {
