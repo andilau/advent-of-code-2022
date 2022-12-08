a18e612325f5cf428b1913ed730d59ee3baece14
Andi Lau <andi@herrlau.de>
2022-12-08 22:58:25 +0100

    Add Day 8: Treetop Tree House

--- src/main/kotlin/days/Day8.kt
+++ src/main/kotlin/days/Day8.kt
@@ -5,20 +5,20 @@ package days
     url = "https://adventofcode.com/2022/day/8",
     date = Date(day = 8, year = 2022)
 )
-class Day8(private val trees: List<String>) : Puzzle {
+class Day8(input: List<String>) : Puzzle {
 
-    override fun partOne(): Int = trees.visibleInsideTrees() + trees.outsideTrees()
+    private val trees = input.map { line -> line.map { it.digitToInt() } }
+    override fun partOne(): Int = trees.visibleOutsideTrees() + trees.visibleInsideTrees()
 
-    override fun partTwo(): Int {
-        return (0..trees.lastIndex).flatMap { y ->
+    override fun partTwo(): Int =
+        (0..trees.lastIndex).flatMap { y ->
             (0..trees[y].lastIndex).map { x -> Pair(x, y) }
-        }.map { p -> trees.scenicScoreAt(p.first, p.second) }.max()
-    }
+        }.maxOf { p -> trees.scenicScoreAt(p.first, p.second) }
 
-    private fun List<String>.scenicScoreAt(x: Int, y: Int): Int {
+    private fun List<List<Int>>.scenicScoreAt(x: Int, y: Int): Int {
         val line = get(y)
-        val left = line.substring(0 until x).reversed()
-        val right = line.substring(x + 1..line.lastIndex)
+        val left = (x - 1 downTo 0).map { line[it] }
+        val right = (x + 1..line.lastIndex).map { line[it] }
         val top = (y - 1 downTo 0).map { this[it][x] }
         val down = (y + 1..lastIndex).map { this[it][x] }
 
@@ -29,24 +29,16 @@ class Day8(private val trees: List<String>) : Puzzle {
         return ls * rs * ts * ds
     }
 
-    private fun List<String>.outsideTrees() = 2 * (size + first().count() - 2)
-    private fun List<String>.visibleInsideTrees() =
+    private fun List<List<Int>>.visibleOutsideTrees() = 2 * (size + first().count() - 2)
+
+    private fun List<List<Int>>.visibleInsideTrees() =
         (1 until this.lastIndex).flatMap { dy ->
             (1 until this[dy].lastIndex).map { dx -> Pair(dx, dy) }
         }.count { (x, y) -> this.visibleAt(x, y) }
 
-    private fun List<String>.visibleAt(x: Int, y: Int): Boolean {
-        val line = get(y)
-        val left = line.substring(0 until x)
-        val right = line.substring(x + 1..line.lastIndex)
-        val top = (0 until y).map { this[it][x] }
-        val down = (y + 1..lastIndex).map { this[it][x] }
-
-        return left.all { it < line[x] } || right.all { it < line[x] } || top.all { it < line[x] } || down.all { it < line[x] }
-    }
-
-
+    private fun List<List<Int>>.visibleAt(x: Int, y: Int) =
+        (0 until x).map { get(y)[it] }.all { it < get(y)[x] }
+                || (x + 1..get(y).lastIndex).map { get(y)[it] }.all { it < get(y)[x] }
+                || (0 until y).map { this[it][x] }.all { it < get(y)[x] }
+                || (y + 1..lastIndex).map { this[it][x] }.all { it < get(y)[x] }
 }
-
-
-
