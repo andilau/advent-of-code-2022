0abf9f67ddaefb279af4e44d836586d330bf7d1d
Andi Lau <andi@herrlau.de>
2022-12-11 18:40:59 +0100

    Add Day 11: Monkey in the Middle

--- src/main/kotlin/days/Day11.kt
+++ src/main/kotlin/days/Day11.kt
@@ -21,14 +21,14 @@ class Day11(val input: String) : Puzzle {
             for (monkey in monkeys) {
                 with(monkey) {
                     while (true) {
-                        val worry = items.removeFirstOrNull() ?: break
+                        val worry = worries.removeFirstOrNull() ?: break
                         inspected++
                         val opWorry = op(worry)
                         val reducedWorry = if (relief > 1) opWorry / relief else opWorry
                         val modWorry = reducedWorry % lcm
 
                         val index = if (modWorry % test == 0L) throwTrue else throwFalse
-                        monkeys[index].items.addLast(modWorry)
+                        monkeys[index].worries.addLast(modWorry)
                     }
                 }
             }
@@ -39,11 +39,11 @@ class Day11(val input: String) : Puzzle {
     private fun List<Monkey>.twoMostActiveMnkeysInspectedMultiplied() =
         map { it.inspected.toLong() }.sortedByDescending { it }.take(2).reduce { a, b -> a * b }
 
-    private fun List<Monkey>.deepCopy() = map { it.copy(items = ArrayDeque(it.items)) }
+    private fun List<Monkey>.deepCopy() = map { it.copy(worries = ArrayDeque(it.worries)) }
 
     data class Monkey(
         val id: Int,
-        val items: ArrayDeque<Long>,
+        val worries: ArrayDeque<Long>,
         val op: (Long) -> Long,
         val test: Int,
         val throwTrue: Int,
@@ -55,7 +55,7 @@ class Day11(val input: String) : Puzzle {
             fun from(it: List<String>): Monkey {
                 assert(it.size == 6)
                 val id = it[0].substringAfter("Monkey ").substringBefore(":").toInt()
-                val items: ArrayDeque<Long> =
+                val worries: ArrayDeque<Long> =
                     it[1].substringAfter("Starting items: ").split(", ").map { it.toLong() }.let {
                         ArrayDeque(it.toList())
                     }
@@ -69,7 +69,7 @@ class Day11(val input: String) : Puzzle {
                 val test = it[3].substringAfter("Test: divisible by ").toInt()
                 val throwTrue = it[4].substringAfter("If true: throw to monkey ").toInt()
                 val throwFalse = it[5].substringAfter("If false: throw to monkey ").toInt()
-                return Monkey(id, items, op, test, throwTrue, throwFalse)
+                return Monkey(id, worries, op, test, throwTrue, throwFalse)
             }
         }
     }
