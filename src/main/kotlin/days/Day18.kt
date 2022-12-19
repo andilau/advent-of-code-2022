b2b359c12936ca5c5b0e2d5617bdece8120f4e6f
Andi Lau <andi@herrlau.de>
2022-12-19 10:04:31 +0100

    Add Day 18: Boiling Boulders
    Faster solution Part2

--- src/main/kotlin/days/Day18.kt
+++ src/main/kotlin/days/Day18.kt
@@ -1,5 +1,7 @@
 package days
 
+import java.util.ArrayDeque
+
 @AdventOfCodePuzzle(
     name = "Boiling Boulders",
     url = "https://adventofcode.com/2022/day/18",
@@ -12,7 +14,22 @@ class Day18(input: List<String>) : Puzzle {
     override fun partOne() = droplets.sumOf { (it.neighbors() subtract droplets).count() }
 
     override fun partTwo(): Int {
-        return 0
+        var outsideFacing = 0
+        val rangeX = droplets.minOf { it.x } - 1..droplets.maxOf { it.x } + 1
+        val rangeY = droplets.minOf { it.y } - 1..droplets.maxOf { it.y } + 1
+        val rangeZ = droplets.minOf { it.z } - 1..droplets.maxOf { it.z } + 1
+
+        val inspect = ArrayDeque<Point3D>().apply { add(Point3D(rangeX.first, rangeY.first, rangeZ.first)) }
+        val seen = mutableSetOf<Point3D>()
+        while (inspect.isNotEmpty()) {
+            val pop = inspect.pop()
+            if (pop in seen) continue
+
+            seen += pop
+            pop.neighbors().filter { it.x in rangeX && it.y in rangeY && it.z in rangeZ }
+                .forEach { side -> if (side in droplets) outsideFacing++ else inspect.add(side) }
+        }
+        return outsideFacing
     }
 
     data class Point3D(val x: Int, val y: Int, val z: Int) {
